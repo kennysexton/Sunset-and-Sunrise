2816c02c0036d4dd611e2a34fe370258225dd8b1
kennysexton <kennysexton4@gmail.com>
2020-09-02 07:20:41 -0400

    Using Timber for logging

--- app/src/main/java/com/kennysexton/sunset/MainActivity.kt
+++ app/src/main/java/com/kennysexton/sunset/MainActivity.kt
@@ -2,11 +2,9 @@ package com.kennysexton.sunset
 
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kennysexton.sunset.model.Movie
@@ -14,6 +12,7 @@ import com.kennysexton.sunset.model.OpenWeatherAPI
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +21,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Timber
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
         setContentView(R.layout.activity_main)
 
 //        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
@@ -29,28 +33,28 @@ class MainActivity : AppCompatActivity() {
 //                    .setAction("Action", null).show()
 //        }
 
-        Log.d("test", "test")
+        Timber.d("test")
         val apiInterface = OpenWeatherAPI.create().getMovies(BuildConfig.OPEN_WEATHER_KEY)
-        val items : ArrayList<String> = ArrayList()
+        val items: ArrayList<String> = ArrayList()
         items.add("list1")
         items.add("list2")
 
         recyclerView = findViewById(R.id.recycler)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerAdapter = RecyclerAdapter(items,this)
+        recyclerAdapter = RecyclerAdapter(items, this)
         recyclerView.adapter = recyclerAdapter
-        Log.d("test", items.toString())
+        Timber.d(items.toString())
 
 
-        apiInterface.enqueue( object : Callback<List<Movie>> {
+        apiInterface.enqueue(object : Callback<List<Movie>> {
             override fun onResponse(call: Call<List<Movie>>?, response: Response<List<Movie>>?) {
 
-                if(response?.body() != null)
-                    Log.d("test", response.body()!!.toString())
+                if (response?.body() != null)
+                    Timber.d(response.body()!!.toString())
             }
 
             override fun onFailure(call: Call<List<Movie>>?, t: Throwable?) {
-                Log.e("test", t.toString())
+                Timber.e(t.toString())
             }
         })
     }
