c0c0e6fc8f30db65c2f7f06d9806f58370de53ba
kennysexton <kennysexton4@gmail.com>
2020-10-03 08:23:31 -0400

    Adding a swipe to delete

--- app/src/main/java/com/kennysexton/sunset/MainActivity.kt
+++ app/src/main/java/com/kennysexton/sunset/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.kennysexton.sunset
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -17,8 +22,11 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var recyclerView: RecyclerView
-    lateinit var recyclerAdapter: RecyclerAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerAdapter: RecyclerAdapter
+
+    // Color behind the rows of our recycle view
+    private var swipeBackground: ColorDrawable = ColorDrawable(Color.parseColor("#F24D4D"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,27 +37,32 @@ class MainActivity : AppCompatActivity() {
         }
         setContentView(R.layout.activity_main)
 
+        // FOB
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Add a new location", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
 
-        val apiInterface = OpenWeatherAPI.create().getCurrentWeather(BuildConfig.OPEN_WEATHER_KEY, "imperial") //TODO move imperial to shared preferences
+        // Recycler init
+        recyclerView = findViewById(R.id.locationsList)
+
+        val apiInterface = OpenWeatherAPI.create().getCurrentWeather(
+            BuildConfig.OPEN_WEATHER_KEY,
+            "imperial"
+        ) //TODO move imperial to shared preferences
         val items: ArrayList<WeatherResponse> = ArrayList()
 
-        recyclerView = findViewById(R.id.recycler)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerAdapter = RecyclerAdapter(items, this)
-        recyclerView.adapter = recyclerAdapter
-        Timber.d(items.toString())
 
         apiInterface.enqueue(object : Callback<WeatherResponse> {
-            override fun onResponse(call: Call<WeatherResponse>?, response: Response<WeatherResponse>?) {
+            override fun onResponse(
+                call: Call<WeatherResponse>?,
+                response: Response<WeatherResponse>?
+            ) {
 
                 if (response?.body() != null) {
                     Timber.d(response.body()!!.toString())
                     response.body()?.let { items.add(it) }
-                    recyclerAdapter.notifyDataSetChanged()
+                    showData(items)
                 }
             }
 
@@ -74,5 +87,137 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    /**
+     * Populates the recyclerView from Earthquake List
+     * @param List of Earthquakes
+     */
+    private fun showData(items: ArrayList<WeatherResponse>) {
+            recyclerView.apply {
+                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                recyclerAdapter = RecyclerAdapter(items, this@MainActivity)
+                recyclerView.adapter = recyclerAdapter
+                Timber.d(items.toString())
+
+            // Initialize Delete Icon
+            val deleteIcon = ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_trash)!!
+
+
+            // Swipe to delete
+            val itemTouchHelperCallback = object :
+                ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+                /**
+                 * One of itemTouchHelper's functions.  Not used in this example
+                 * @return Boolean
+                 */
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                /**
+                 * Detects a swipe on a RecyclerView row
+                 * @param viewHolder Our recyclerView
+                 * @param positon in the adapter
+                 */
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, positon: Int) {
+                    (adapter as RecyclerAdapter).removeItem(viewHolder as RecyclerAdapter.ViewHolder)
+                }
+
+                /**
+                 * Programmatically generated view behind each row
+                 */
+                override fun onChildDraw(
+                    c: Canvas,
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    dX: Float,
+                    dY: Float,
+                    actionState: Int,
+                    isCurrentlyActive: Boolean
+                ) {
+                    val rowRectangle = viewHolder.itemView
+
+                    // Calculate vertical margins for delete Icon
+                    val iconMargin = (rowRectangle.height - deleteIcon.intrinsicHeight) / 2
+
+                    // Swiped Right
+                    if (dX > 0) {
+                        swipeBackground.setBounds(
+                            rowRectangle.left,
+                            rowRectangle.top,
+                            dX.toInt(),
+                            rowRectangle.bottom
+                        )
+                        deleteIcon.setBounds(
+                            rowRectangle.left + iconMargin,
+                            rowRectangle.top + iconMargin,
+                            rowRectangle.left + iconMargin + deleteIcon.intrinsicWidth,
+                            rowRectangle.bottom - iconMargin
+                        )
+                    }
+                    // Swiped Left
+                    else {
+                        swipeBackground.setBounds(
+                            rowRectangle.right + dX.toInt(),
+                            rowRectangle.top,
+                            rowRectangle.right,
+                            rowRectangle.bottom
+                        )
+                        deleteIcon.setBounds(
+                            rowRectangle.right - iconMargin - deleteIcon.intrinsicWidth,
+                            rowRectangle.top + iconMargin,
+                            rowRectangle.right - iconMargin,
+                            rowRectangle.bottom - iconMargin
+                        )
+                    }
+
+
+                    // Draw the red background
+                    swipeBackground.draw(c)
+
+                    c.save()
+
+                    // Clipping so icon appears behind row
+                    if (dX > 0) {
+                        c.clipRect(
+                            rowRectangle.left,
+                            rowRectangle.top,
+                            dX.toInt(),
+                            rowRectangle.bottom
+                        )
+                    } else {
+                        c.clipRect(
+                            rowRectangle.right + dX.toInt(),
+                            rowRectangle.top,
+                            rowRectangle.right,
+                            rowRectangle.bottom
+                        )
+                    }
+
+                    deleteIcon.draw(c)
+                    c.restore()
+
+                    super.onChildDraw(
+                        c,
+                        recyclerView,
+                        viewHolder,
+                        dX,
+                        dY,
+                        actionState,
+                        isCurrentlyActive
+                    )
+                }
+            }
+            val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+            itemTouchHelper.attachToRecyclerView(recyclerView)
+
+        }
+
     }
 }
--- app/src/main/java/com/kennysexton/sunset/RecyclerAdapter.kt
+++ app/src/main/java/com/kennysexton/sunset/RecyclerAdapter.kt
@@ -1,12 +1,14 @@
 package com.kennysexton.sunset
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.kennysexton.sunset.model.WeatherResponse
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
@@ -17,7 +19,7 @@ import kotlin.math.roundToInt
 const val weatherIconPath = "https://openweathermap.org/img/wn/"
 const val weatherIconEnding = "@2x.png"
 
-class RecyclerAdapter(private val items : ArrayList<WeatherResponse>, private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+class RecyclerAdapter(private val items : ArrayList<WeatherResponse>, private val context: Context) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     override fun getItemCount(): Int {
         return items.size
@@ -28,6 +30,7 @@ class RecyclerAdapter(private val items : ArrayList<WeatherResponse>, private va
         return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_row, parent, false))
     }
 
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val iconPath = "$weatherIconPath${items[position].weather[0].icon}$weatherIconEnding"
         Timber.d("Loading icon from path: $iconPath")
@@ -35,7 +38,7 @@ class RecyclerAdapter(private val items : ArrayList<WeatherResponse>, private va
         holder.location.text = items[position].name
 //        holder.weatherDescription.text = items[position].weather[0].description
 
-        holder.temperature.text = items[position].main.temp.roundToInt().toString()
+        holder.temperature.text = context.resources.getString(R.string.weatherDegrees, items[position].main.temp.roundToInt())
         Picasso.get().load(iconPath).into(holder.weatherIcon , object : Callback {
             override fun onSuccess() {
                 Timber.d("Picasso image load successful")
@@ -47,12 +50,42 @@ class RecyclerAdapter(private val items : ArrayList<WeatherResponse>, private va
             }
         })
     }
+
+    /**
+     * Function for deleting rows via swipe action
+     * @param viewHolder
+     */
+    fun removeItem(viewHolder: ViewHolder){
+        val removedPosition = viewHolder.adapterPosition
+        val removedItem = items[viewHolder.adapterPosition]
+        val removedTitle = removedItem.name
+
+        items.removeAt(removedPosition)
+        notifyItemRemoved(removedPosition)
+
+
+        // Log the row deletion
+        Timber.d("$removedTitle deleted")
+
+
+        // Snack bar to let the user know that they deleted a row
+        Snackbar.make(viewHolder.itemView, "$removedTitle  deleted", Snackbar.LENGTH_LONG).setAction("UNDO"){
+            items.add(removedPosition, removedItem)
+            notifyItemInserted(removedPosition)
+
+            // Write to the log that an undo
+            Timber.d("$removedTitle restored")
+        }.show()
+    }
+
+    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+        val location: TextView = view.location
+        //    val weatherDescription: TextView = view.weatherDescription
+        val temperature: TextView = view.temperature
+        val weatherIcon: ImageView = view.weatherIcon
+    }
+
+
 }
 
-class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-    val location: TextView = view.location
-//    val weatherDescription: TextView = view.weatherDescription
-    val temperature: TextView = view.temperature
-    val weatherIcon: ImageView = view.weatherIcon
 
-}
