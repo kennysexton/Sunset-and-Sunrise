3b99b74d85f2f8a4b22091e35cb639a17b57b0e8
kennysexton <kennysexton4@gmail.com>
2024-09-04 16:56:55 -0600

    Default font family via compose theming

--- app/src/main/java/com/kennysexton/sunset/ui/components/WeatherLandingScreen.kt
+++ app/src/main/java/com/kennysexton/sunset/ui/components/WeatherLandingScreen.kt
@@ -1,14 +1,11 @@
 package com.kennysexton.sunset.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
--- app/src/main/java/com/kennysexton/sunset/ui/theme/Type.kt
+++ app/src/main/java/com/kennysexton/sunset/ui/theme/Type.kt
@@ -2,33 +2,57 @@ package com.kennysexton.sunset.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.kennysexton.sunset.R
 
-// Set of Material typography styles to start with
+object AppFont {
+    val Monsterrat = FontFamily(
+        Font(R.font.montserrat_medium, FontWeight.Medium),
+        Font(R.font.montserrat_regular, FontWeight.Normal),
+        Font(R.font.montserrat_light, FontWeight.Light),
+        Font(R.font.montserrat_extra_light, FontWeight.ExtraLight)
+    )
+}
+
+
+private val defaultTypography = Typography()
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = AppFont.Monsterrat),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = AppFont.Monsterrat),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = AppFont.Monsterrat),
+
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = AppFont.Monsterrat),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = AppFont.Monsterrat),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = AppFont.Monsterrat),
+
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = AppFont.Monsterrat),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = AppFont.Monsterrat),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = AppFont.Monsterrat),
+
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = AppFont.Monsterrat),
+    bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = AppFont.Monsterrat),
+    bodySmall = defaultTypography.bodySmall.copy(fontFamily = AppFont.Monsterrat),
+
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = AppFont.Monsterrat),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = AppFont.Monsterrat),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = AppFont.Monsterrat)
 )
+/* Other default text styles to override
+titleLarge = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 22.sp,
+    lineHeight = 28.sp,
+    letterSpacing = 0.sp
+),
+labelSmall = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Medium,
+    fontSize = 11.sp,
+    lineHeight = 16.sp,
+    letterSpacing = 0.5.sp
+)
+*/
