72299cd98b1cfd5a5fde701f2f4691d8577165e0
kennysexton <kennysexton4@gmail.com>
2024-09-04 22:46:26 -0600

    spending a lot of time trying to add Button() features, but gradient is only supported as a modifier :(

--- app/src/main/java/com/kennysexton/sunset/ui/components/Buttons.kt
+++ app/src/main/java/com/kennysexton/sunset/ui/components/Buttons.kt
@@ -5,18 +5,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonElevation
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
@@ -25,12 +24,13 @@ import androidx.compose.ui.unit.dp
 import com.kennysexton.sunset.ui.theme.HappyFisherGradientEnd
 import com.kennysexton.sunset.ui.theme.HappyFisherGradientStart
 
+
 @Composable
 fun DefaultButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    shape: Shape = MaterialTheme.shapes.medium,
+    shape: Shape = RoundedCornerShape(6.dp),
     colors: ButtonColors
     = ButtonDefaults.buttonColors(),
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
@@ -60,31 +60,40 @@ fun DefaultButton(
 @Composable
 fun BlueButton(
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
-        .padding(16.dp)
-        .clip(shape = RoundedCornerShape(8.dp))
-        .background(
-            brush = Brush.linearGradient(
-                colors = listOf(HappyFisherGradientStart, HappyFisherGradientEnd)
-            )
-        ),
+    modifier: Modifier = Modifier,
     content: @Composable (RowScope) -> Unit,
 ) {
     DefaultButton(
-        shape = RoundedCornerShape(6.dp),
-        colors = ButtonColors(containerColor = Color.Transparent, contentColor = Color.White, disabledContentColor = Color.Transparent, disabledContainerColor = Color.White),
+
+        colors = ButtonColors(
+            containerColor = Color.Transparent,
+            contentColor = Color.White,
+            disabledContentColor = Color.Transparent,
+            disabledContainerColor = Color.White
+        ),
         onClick = onClick,
-        modifier = modifier,
+        // Custom styling, but also supports modifiers added at the caller
+        modifier = modifier.then(
+            Modifier
+                .padding(16.dp)
+                .clip(shape = RoundedCornerShape(8.dp))
+                .shadow(elevation = 8.dp)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(HappyFisherGradientStart, HappyFisherGradientEnd)
+                    )
+                )
+        ),
         content = content
+
     )
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun BlueButtonPreview() {
     BlueButton(onClick = {}) {
         Text(text = "Blue button")
     }
-
 }
--- app/src/main/java/com/kennysexton/sunset/ui/components/WeatherLandingScreen.kt
+++ app/src/main/java/com/kennysexton/sunset/ui/components/WeatherLandingScreen.kt
@@ -1,10 +1,11 @@
 package com.kennysexton.sunset.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScopeInstance.align
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,8 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
 import com.kennysexton.sunset.model.Coord
 import com.kennysexton.sunset.model.Main
@@ -25,16 +28,28 @@ import kotlin.math.roundToInt
 
 @Composable
 fun WeatherLandingScreen(innerPadding: PaddingValues, weatherList: List<WeatherResponse>) {
-    LazyColumn {
-        items(weatherList) { location ->
-            WeatherListItem(
-                weather = location,
-                modifier = Modifier.padding(innerPadding)
-            )
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(innerPadding))
+    {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(weatherList) { location ->
+                WeatherListItem(
+                    weather = location,
+                )
+            }
         }
-    }
-    BlueButton(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.BottomEnd).padding(innerPadding)) {
-        Text(text = "Add Location")
+
+        BlueButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .padding(innerPadding)
+                .align(Alignment.BottomEnd)
+        ) {
+            Text(text = "Add Location")
+        }
+
+
     }
 }
 
@@ -71,7 +86,6 @@ fun WeatherListItem(weather: WeatherResponse, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun WeatherListItemPreview() {
-
     val sampleWeatherList = listOf(
         WeatherResponse(
             coord = Coord("39.9523", "-75.1638"),
